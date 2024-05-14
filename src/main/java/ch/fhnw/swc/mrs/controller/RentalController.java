47d2a705dbd2ceb06c30a9188289a461686382e1
Livio Jäckle <livio.jaeckle@students.fhnw.ch>
2024-05-14 21:48:22 +0200

    implement the RentalController class

--- src/main/java/ch/fhnw/swc/mrs/controller/RentalController.java
+++ src/main/java/ch/fhnw/swc/mrs/controller/RentalController.java
@@ -1,4 +1,116 @@
 package ch.fhnw.swc.mrs.controller;
 
-public class RentalController {
+import static ch.fhnw.swc.mrs.util.JsonUtil.dataToJson;
+import static ch.fhnw.swc.mrs.util.RequestUtil.getParamId;
+import static spark.Spark.get;
+import static spark.Spark.delete;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+
+import ch.fhnw.swc.mrs.util.JsonUtil;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import ch.fhnw.swc.mrs.api.MRSServices;
+import ch.fhnw.swc.mrs.model.Rental;
+import ch.fhnw.swc.mrs.util.StatusCodes;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public final class RentalController {
+
+    private static MRSServices backend;
+
+    private static Route fetchAllRentals = (Request request, Response response) -> {
+        Collection<Rental> rentals = backend.getAllRentals();
+        return dataToJson(rentals);
+    };
+
+//    private static Route createRental = (Request request, Response response) -> {
+//        Rental rental = (Rental) jsonToData(request.body(), Rental.class);
+//        Rental createdRental = backend.createRental(rental.getUser(), rental.getMovie(), rental.getRentalDate());
+//        if (createdRental == null) {
+//            halt(StatusCodes.BAD_REQUEST);
+//        }
+//        response.status(StatusCodes.CREATED);
+//        return dataToJson(createdRental);
+//    };
+
+    private static Route deleteRental = (Request request, Response response) -> {
+        long id = getParamId(request);
+        if (backend.deleteRental(id)) {
+            response.status(StatusCodes.NO_CONTENT);
+        } else {
+            response.status(StatusCodes.NOT_FOUND);
+        }
+        return "";
+    };
+
+    /**
+     * Initialize RentalController by registering back-end and routes.
+     *
+     * @param services the back-end component.
+     */
+    public static void init(MRSServices services) {
+        if (services == null) {
+            throw new IllegalArgumentException("Backend component missing");
+        }
+        backend = services;
+        JsonUtil.registerSerializer(new RentalSerializer());
+        JsonUtil.registerDeserializer(Rental.class, new RentalDeserializer());
+
+        get("/rentals", RentalController.fetchAllRentals);
+//        post("/rentals", RentalController.createRental);
+        delete("/rentals/:id", RentalController.deleteRental);
+    }
+
+    // Prevent instantiation
+    private RentalController() {
+    }
+
+    /**
+     * Helper class to serialize the Rental object to JSON
+     */
+    private static class RentalSerializer extends StdSerializer<Rental> {
+        protected RentalSerializer() {
+            super(Rental.class);
+        }
+
+        @Override
+        public void serialize(Rental rental, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+            jgen.writeStartObject();
+            jgen.writeNumberField("id", rental.getRentalId());
+            jgen.writeNumberField("userId", rental.getUser().getUserid());
+            jgen.writeNumberField("movieId", rental.getMovie().getMovieid());
+            jgen.writeStringField("rentalDate", rental.getRentalDate().format(DateTimeFormatter.ISO_DATE));
+            jgen.writeEndObject();
+        }
+    }
+
+    /**
+     * Helper class to deserialize the Rental object from JSON
+     */
+    private static class RentalDeserializer extends StdDeserializer<Rental> {
+        protected RentalDeserializer() {
+            super(Rental.class);
+        }
+
+        @Override
+        public Rental deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+            JsonNode node = jp.getCodec().readTree(jp);
+            long userId = node.get("userId").asLong();
+            long movieId = node.get("movieId").asLong();
+            LocalDate rentalDate = LocalDate.parse(node.get("rentalDate").asText(), DateTimeFormatter.ISO_DATE);
+            return new Rental(backend.getUserById(userId), backend.getMovieById(movieId), rentalDate);
+        }
+    }
 }
