b1448d47896f25f734f86ccec088aff9350d65ec
Livio Jäckle <livio.jaeckle@students.fhnw.ch>
2024-06-07 21:50:38 +0200

    add a delete function

---
+++ src/test/java/ch/fhnw/swc/mrs/fixture/MRSServicesFactory.java
@@ -0,0 +1,23 @@
+package ch.fhnw.swc.mrs.fixture;
+
+import ch.fhnw.swc.mrs.data.SimpleMRSServices;
+
+/**
+ * Helper class to allow opening db only once in an application.
+ *
+ */
+public final class MRSServicesFactory {
+    private static SimpleMRSServices mrsServices = new SimpleMRSServices();
+
+    // make constructor private
+    private MRSServicesFactory() {
+    }
+
+    static {
+        mrsServices.createDB();
+    }
+
+    static SimpleMRSServices getInstance() {
+        return mrsServices;
+    }
+}
